49d46ffc028bbe3031c68aaf0b4b552b037d6b25
peterjxl <peterjxl@qq.com>
2022-03-31 22:36:18 +0800

    项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'peterjxl\'s blog', head: [
+    ['link', { rel: 'icon', href: 'https://open.weixin.qq.com/qr/code?username=a_javaboy' }]
+  ],
+  themeConfig: {
+    logo: 'https://open.weixin.qq.com/qr/code?username=a_javaboy',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '国际站', link: 'http://www.javaboy.org' },
+      { text: '国内站', link: 'http://www.itboyhub.com' }
+    ],
+    sidebar: 'auto'
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: https://open.weixin.qq.com/qr/code?username=a_javaboy
+heroText: 江南一点雨
+tagline: Java 修炼手册
+actionText: 开始学习 →
+actionLink: /springboot/
+---
