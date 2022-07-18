87f8ec0dae5eb26f011e1c60f93c2217b48f0b46
peterjxl <peterjxl@qq.com>
2022-07-18 22:47:17 +0800

    各菜单路径信息简单梳理

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,44 @@
 module.exports = {
   title: '程序员的网络日志',
   head: [
-    ['link', { rel: 'icon', href: 're0.jpg' }]
+    ['link', { rel: 'icon', href: '/re0.jpg' }]
   ],
   themeConfig: {
-    logo: 're0.jpg',
+    logo: '/re0.jpg',
     nav: [
       { text: '首页', link: '/' },
       {
         text: '编程', ariaLabel: 'program',
         items: [
-          { text: '计算机科学导论', link: '/' },
-          { text: '数字电路', link: '/DigitalCircuit/' },
-          { text: '计算机组成原理', link: '/' },
-          { text: '操作系统', link: '/' },
-          { text: 'Linux', link: '/' },
-          { text: '数据结构', link: '/' },
-          { text: '编译原理', link: '/' },
-          { text: '计算机网络', link: '/' },
-          { text: '数据库', link: '/' },
-          { text: '前端', link: '/' },
+          { text: '计算机科学导论', link: '/ComputerScience/' },
+          { text: '数字电路', link: '/ComputerDigitalCircuit/' },
+          { text: '计算机组成原理', link: '/ComputerPrinciplesOfComputer/' },
+          { text: '操作系统', link: '/ComputerOS/' },
+          { text: 'Linux', link: '/ComputerLinux/' },
+          { text: '数据结构', link: '/ComputerDataStructer/' },
+          { text: '编译原理', link: '/ComputerCompilationPrinciple' },
+          { text: '计算机网络', link: '/ComputerNetwork' },
+          { text: '数据库', link: '/ComputerDataBase' },
+          { text: '前端', link: '/Computerfontend' },
         ]
       },
       {
-        text: '效率软件推荐', ariaLabel: 'program',
+        text: '效率软件推荐', ariaLabel: 'softWare',
         items: [
-          { text: '笔记软件', link: '/' },
-          { text: 'Quicker', link: '/' },
-          { text: 'Qttabar', link: '/' },
-          { text: 'Wgesture', link: '/' },          
-          { text: '浏览器与插件', link: '/' },
-          { text: '视频播放器', link: '/' },
-          { text: '待办清单', link: '/' },
-          { text: '终端软件', link: '/' },
-          { text: 'uTools', link: '/' },
-          { text: '番茄盒子', link: '/' },
+          { text: '笔记软件', link: '/SoftwareNote' },
+          { text: 'Quicker', link: '/SoftwareQuicker' },
+          { text: 'Qttabar', link: '/SoftwareQttabar' }, 
+          { text: 'Wgesture', link: '/SoftwareWgesture' },          
+          { text: '浏览器与插件', link: '/SoftwareBrowser' },
+          { text: '视频播放器', link: '/SoftwareVideo' },
+          { text: '待办清单', link: '/SoftwareTodo' },
+          { text: '终端软件', link: '/SoftwareCMD' },
+          { text: 'uTools', link: '/SoftwareuTools' },
+          { text: '番茄盒子', link: '/SoftwareTomato' },
         ]
       },
       {
-        text: '其他', ariaLabel: 'Language Menu',
+        text: '其他', ariaLabel: 'Other',
         items: [
           { text: '网站日记', link: '/other/blogupdatelog.md' },
           { text: '赞赏支持', link: '/other/reward.md' }
@@ -50,11 +50,11 @@ module.exports = {
     sidebar: [
       {
         title: 'DigitalCircuit',
-        path: '/DigitalCircuit/',
+        path: '/ComputerDigitalCircuit/',
         collapsable: false,
         sidebarDepth: 2,
         children: [
-          '/DigitalCircuit/1Introduce'
+          '/ComputerDigitalCircuit/1Introduce'
         ]
       }
     ]
