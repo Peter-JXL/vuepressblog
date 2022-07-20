5b209399dbdc83a1f6ba54aea9bad49320d49641
peterjxl <peterjxl@qq.com>
2022-07-20 22:17:45 +0800

    博客相关笔记链路调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,36 +12,38 @@ module.exports = {
         items: [
           { text: '计算机科学导论', link: '/ComputerScience/' },
           { text: '数字电路', link: '/ComputerDigitalCircuit/' },
-          { text: '计算机组成原理', link: '/ComputerPrinciplesOfComputer/' },
+          { text: '计算机组成原理', link: '/ComputerOrganization/' },
           { text: '操作系统', link: '/ComputerOS/' },
           { text: 'Linux', link: '/ComputerLinux/' },
           { text: '数据结构', link: '/ComputerDataStructer/' },
-          { text: '编译原理', link: '/ComputerCompilationPrinciple' },
-          { text: '计算机网络', link: '/ComputerNetwork' },
-          { text: '数据库', link: '/ComputerDataBase' },
-          { text: '前端', link: '/Computerfontend' },
+          { text: '编译原理', link: '/ComputerCompilation/' },
+          { text: '计算机网络', link: '/ComputerNetwork/' },
+          { text: '数据库', link: '/ComputerDataBase/' },
+          { text: '前端', link: '/Computerfontend/' },
+          { text: 'Java', link: '/ComputerJava/' },
         ]
       },
       {
         text: '效率软件推荐', ariaLabel: 'softWare',
         items: [
-          { text: '笔记软件', link: '/SoftwareNote' },
-          { text: 'Quicker', link: '/SoftwareQuicker' },
-          { text: 'Qttabar', link: '/SoftwareQttabar' }, 
-          { text: 'Wgesture', link: '/SoftwareWgesture' },          
-          { text: '浏览器与插件', link: '/SoftwareBrowser' },
-          { text: '视频播放器', link: '/SoftwareVideo' },
-          { text: '待办清单', link: '/SoftwareTodo' },
-          { text: '终端软件', link: '/SoftwareCMD' },
-          { text: 'uTools', link: '/SoftwareuTools' },
-          { text: '番茄盒子', link: '/SoftwareTomato' },
+          { text: '笔记软件', link: '/SoftwareNote/' },
+          { text: 'Quicker', link: '/SoftwareQuicker/' },
+          { text: 'Qttabar', link: '/SoftwareQttabar/' }, 
+          { text: 'Wgesture', link: '/SoftwareWgesture/' },          
+          { text: '浏览器与插件', link: '/SoftwareBrowser/' },
+          { text: '视频播放器', link: '/SoftwareVideo/' },
+          { text: '待办清单', link: '/SoftwareTodo/' },
+          { text: '终端软件', link: '/SoftwareCMD/' },
+          { text: 'uTools', link: '/SoftwareuTools/' },
+          { text: '番茄盒子', link: '/SoftwareTomato/' },
         ]
       },
       {
         text: '其他', ariaLabel: 'Other',
         items: [
           { text: '网站日记', link: '/other/blogupdatelog.md' },
-          { text: '赞赏支持', link: '/other/reward.md' }
+          { text: '赞赏支持', link: '/other/reward.md' },
+          { text: '关于作者', link: '/other/about.md' }
         ]
       },
       { text: 'Gitee', link: 'https://gitee.com/peterjxl/vuepressblog' },
