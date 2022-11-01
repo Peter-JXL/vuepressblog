6cc4157d524d358f134ea9a7a10bf4ab7672e63c
peterjxl <peterjxl@qq.com>
2022-11-01 21:51:58 +0800

    更换首页大图

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,11 +29,11 @@ module.exports = {
       indexName: 'peterjxl'
     },
 
-    bodyBgImg: 'https://image.peterjxl.com/blog/re-background.jpg',  // 你的图片路径(必须位于 public 下)，可以是 URL
+    bodyBgImg: 'https://image.peterjxl.com/blog/Emilia.jpg',  // 你的图片路径(必须位于 public 下)，可以是 URL
     bodyBgImgOpacity: 1, // body 背景图透明度，选值 0 ~ 1.0, 默认0.5
 
     indexImg: {
-      navColor: 2,    // 导航栏左侧名字、中间搜索框、右侧字体的颜色，1 是黑色，2 是白色。默认是 1
+      navColor: 1,    // 导航栏左侧名字、中间搜索框、右侧字体的颜色，1 是黑色，2 是白色。默认是 1
       switchNavColor: true,    // 页面移出大图片的位置后，navColor 是否变换，如由白色变黑色，黑色变白色。默认是 false
       // 因为本主题的默认背景色偏向白色，如果 navColor 是 2，建议需要开启(true)，否则白背景 + 白字体 = 看不见
       bgTimeColor: true,     // 是否开启图片的背景色随一天的不同时间而变化，并且开启时间窗口提示，默认是 false。时间分为四种：白天（原图）、黄昏（偏黄）、晚上（偏黑）、深夜（偏深黑）
