68f0169576ff3e597fbb42c49cf4c3f1f56dafc4
peterjxl <peterjxl@qq.com>
2022-11-04 07:27:41 +0800

    :art: 取消欢迎语滚动、修改x86asm的代码language为nasm（用于高亮）

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,6 +30,7 @@ module.exports = {
     },
 
     bodyBgImg: 'https://image.peterjxl.com/blog/re-background.jpg',  // 你的图片路径(必须位于 public 下)，可以是 URL
+    // bodyBgImg: '/img/Emilia.jpg',  // 本地测试用，你的图片路径(必须位于 public 下)，可以是 URL
     bodyBgImgOpacity: 1, // body 背景图透明度，选值 0 ~ 1.0, 默认0.5
 
     indexImg: {
@@ -38,8 +39,8 @@ module.exports = {
       // 因为本主题的默认背景色偏向白色，如果 navColor 是 2，建议需要开启(true)，否则白背景 + 白字体 = 看不见
       bgTimeColor: true,     // 是否开启图片的背景色随一天的不同时间而变化，并且开启时间窗口提示，默认是 false。时间分为四种：白天（原图）、黄昏（偏黄）、晚上（偏黑）、深夜（偏深黑）
       bgTimeColorArray: ['transparent', 'rgba(255, 148, 48, .2)', 'rgba(0, 0, 0, .3)', 'rgba(0, 0, 0, .5)'],   // 第一个是白天的颜色（默认原图），第二个是黄昏的颜色，第三个是晚上的颜色，第四个是深夜的颜色。bgTimeColor 为 true 生效。提示：如果不想要这个效果，但是又想要时间窗口提示效果，则改为 ['transparent', 'transparent', 'transparent', 'transparent']
-      descFade: true,   // 是否开启图片中间描述的淡入效果，默认为 false
-      desc: ["那就从零开始。"],  // 多个描述，如果填写则覆盖 config.js 的 description，不填写默认读取 config.js 的 description，descFadeIn 为 true 生效
+      descFade: false,   // 是否开启图片中间描述的淡入效果，默认为 false
+      desc: ["那就从零开始。"],  // 多个描述，如果填写则覆盖 config.js 的 description ，不填写默认读取 config.js 的 description，descFadeIn 为 true 生效
       descFontSize: '1.4rem',   // desc 的字体大小，默认 1.4rem。提示：原主题是 1.1rem
       descFadeInTime: 200,  // 描述的淡入效果持续时间，descFade 为 true 生效，默认 200 毫秒
       descFadeOutTime: 100,  // 描述的淡出效果持续时间，descFade 为 true 生效，默认 100 毫秒
