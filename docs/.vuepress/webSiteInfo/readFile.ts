00fda329544bd5ad336a35d89fc05382928d4413
peterjxl <peterjxl@qq.com>
2022-11-16 22:07:02 +0800

    修改站点信息模块的位置

--- docs/.vuepress/webSiteInfo/readFile.ts
+++ docs/.vuepress/webSiteInfo/readFile.ts
@@ -2,14 +2,14 @@ import fs from 'fs'; // 文件模块
 import path from 'path'; // 路径模块
 import matter from 'gray-matter'; // FrontMatter解析器 https://github.com/jonschlinkert/gray-matter
 import chalk from 'chalk' // 命令行打印美化
-import log from 'console'
+const log = console.log
 const docsRoot = path.join(__dirname, '..', '..', '..', 'docs'); // docs文件路径
 
 /**
  * 获取本站的文章数据
  * 获取所有的 md 文档，可以排除指定目录下的文档
  */
-function readFileList(excludeFiles = [''], dir = docsRoot, filesList = []) {
+function readFileList(excludeFiles: Array<string> = [''], dir: string = docsRoot, filesList: Array<Object> = []) {
   const files = fs.readdirSync(dir);
   files.forEach((item, index) => {
     let filePath = path.join(dir, item);
@@ -53,10 +53,10 @@ function readFileList(excludeFiles = [''], dir = docsRoot, filesList = []) {
  */
 function readTotalFileWords(excludeFiles = ['']) {
   const filesList = readFileList(excludeFiles);
-  var wordCount = 0;
-  filesList.forEach((item) => {
+  let wordCount = 0;
+  filesList.forEach((item: any) => {
     const content = getContent(item.filePath);
-    var len = counter(content);
+    let len = counter(content);
     wordCount += len[0] + len[1];
   });
   if (wordCount < 1000) {
@@ -68,15 +68,15 @@ function readTotalFileWords(excludeFiles = ['']) {
  * 获取每一个文章的字数
  * 可以排除某个目录下的 md 文档字数
  */
-function readEachFileWords(excludeFiles = [''], cn, en) {
+function readEachFileWords(excludeFiles: Array<string> = [''], cn: number, en: number) {
   const filesListWords = [];
   const filesList = readFileList(excludeFiles);
-  filesList.forEach((item) => {
+  filesList.forEach((item: any) => {
     const content = getContent(item.filePath);
-    var len = counter(content);
+    let len = counter(content);
     // 计算预计的阅读时间
-    var readingTime = readTime(len, cn, en);
-    var wordsCount = 0;
+    let readingTime = readTime(len, cn, en);
+    let wordsCount: any = 0;
     wordsCount = len[0] + len[1];
     if (wordsCount >= 1000) {
       wordsCount = Math.round(wordsCount / 100) / 10 + 'k';
@@ -92,30 +92,30 @@ function readEachFileWords(excludeFiles = [''], cn, en) {
 /**
  * 计算预计的阅读时间
  */
-function readTime(len, cn = 300, en = 160) {
-  var readingTime = len[0] / cn + len[1] / en;
+function readTime(len: Array<number>, cn: number = 300, en: number = 160) {
+  let readingTime = len[0] / cn + len[1] / en;
   if (readingTime > 60 && readingTime < 60 * 24) {   // 大于一个小时，小于一天
-    let hour = parseInt(readingTime / 60);
-    let minute = parseInt((readingTime - hour * 60));
+    let hour = Math.trunc(readingTime / 60);
+    let minute = Math.trunc(readingTime - hour * 60);
     if (minute === 0) {
       return hour + 'h';
     }
     return hour + 'h' + minute + 'm';
   } else if (readingTime > 60 * 24) {      // 大于一天
-    let day = parseInt(readingTime / (60 * 24));
-    let hour = parseInt((readingTime - day * 24 * 60) / 60);
+    let day = Math.trunc(readingTime / (60 * 24));
+    let hour = Math.trunc((readingTime - day * 24 * 60) / 60);
     if (hour === 0) {
       return day + 'd';
     }
     return day + 'd' + hour + 'h';
   }
-  return readingTime < 1 ? '1' : parseInt((readingTime * 10)) / 10 + 'm';   // 取一位小数
+  return readingTime < 1 ? '1' : Math.trunc(readingTime * 10) / 10 + 'm';   // 取一位小数
 }
 
 /**
  * 读取文件内容
  */
-function getContent(filePath) {
+function getContent(filePath: string) {
   return fs.readFileSync(filePath, 'utf8');
 }
 /**
@@ -123,13 +123,13 @@ function getContent(filePath) {
  * cn：中文
  * en：一整句英文（没有空格隔开的英文为 1 个）
  */
-function counter(content) {
+function counter(content: string) {
   const cn = (content.match(/[\u4E00-\u9FA5]/g) || []).length;
   const en = (content.replace(/[\u4E00-\u9FA5]/g, '').match(/[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|[\u00E4\u00C4\u00E5\u00C5\u00F6\u00D6]+|\w+/g) || []).length;
   return [cn, en];
 }
 
-module.exports = {
+export {
   readFileList,
   readTotalFileWords,
   readEachFileWords,
