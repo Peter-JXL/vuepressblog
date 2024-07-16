0591fa1ab215a68064e56d91e0a6e4b362ff5461
peterjxl <peterjxl@qq.com>
2024-07-16 21:54:20 +0800

    :memo: update nav

--- docs/.vuepress/config/themeConfig.ts
+++ docs/.vuepress/config/themeConfig.ts
@@ -186,6 +186,7 @@ export default <VdoingThemeConfig>{
         { text: '如何搭建一个博客', link: '/Blog/' },
         { text: '关于邮件服务器', link: '/About/BuildMail/' },
         { text: '本站的分享资料', link: '/About/share/' },
+        { text: '打赏', link: '/About/reward/' },
         { text: '年度总结', items: [
           { text: '2022年度总结', link: '/About/summary-2022/' },
           { text: '2023年度总结', link: '/About/summary-2023/' },
